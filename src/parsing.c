8017262bf123636949d6a3787bf25fe40b8eaeb3
Lucas <lucas.duboisse@epitech.eu>
2019-05-05 18:20:54 +0200

    update parsing

--- src/parsing.c
+++ src/parsing.c
@@ -35,9 +35,9 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 arguments parsing(int ac, char **av)
 {
     arguments args = { NULL, 0, NULL };
-    static struct argp argp = { options, parse_opt, NULL, NULL, NULL, NULL, NULL };
+    struct argp argp = { options, parse_opt, NULL, NULL, NULL, NULL, NULL };
 
-    int test = argp_parse(&argp, ac, av, 0, 0, &args);
+    argp_parse(&argp, ac, av, 0, 0, &args);
     if (ac != 7) {
         fprintf(stderr, "Error: Wrong number of arg.\n");
         exit(84);
